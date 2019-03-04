dde484065a00f9fe24f0cb5ff4a479c637593461
kkeefe <kevinpk@hawaii.edu>
2019-03-04 07:51:21 -1000

    staging from mac 2

--- basic.cpp
+++ basic.cpp
@@ -7,13 +7,15 @@ using namespace std;
 
 int main(int argc, char** argv){
  
+  why is c++ giving me so much shit?
+  
   string word;
   while(word != "exit"){
     cout << " please enter a any string of characters you want to be processed: ";
     getline(cin, word);
     istringstream stream(word);
     cout << word << " -- this is what you entered .." << endl;;
-    
+
     while(stream >> word){
       cout << " new word .. " << endl;
       cout << word << " this is what word is now currently pointing too " << endl;

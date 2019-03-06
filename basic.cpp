a67650bd84c085e4c996a2bb7190cf55f9a29ed0
kkeefe <kevinpk@hawaii.edu>
2019-03-05 21:00:19 -1000

    including some txt files for asic maps..

--- basic.cpp
+++ basic.cpp
@@ -1,19 +1,36 @@
 #include "basic.hh"//includes algorithm, function, vector headers, etc..
 #include <sstream> // 4 is the place where istringstream is located..
 
+
+
 using namespace std; 
 
 
 int main(int argc, char **argv)
 {
-  shared_ptr<string> p1;
-  shared_ptr<list<int>> p2;
 
-  if(p1 and p1->empty()){
-    *p1 = "hi"; 
+  ifstream read_file;
+  read_file.open("pixel_cable.txt");
+
+  ifstream read_file2;
+  read_file2.open("
+  
+  int num;
+  while(read_file >> num){
+    cout << num << endl;
   }
+  
+  //pixel centric approach
+  map<int, int> map_pixel_2cable;
 
-  printf("ni hao\n");
+  // shared_ptr<string> p1;
+  // shared_ptr<list<int>> p2;
+
+  // if(p1 and p1->empty()){
+  //   *p1 = "hi"; 
+  // }
+
+  // printf("ni hao\n");
 
 
   return 0;

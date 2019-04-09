67c0f75a4697baff830a71ff526f1bf4fd8fedaf
kkeefe <kevinpk@hawaii.edu>
2019-04-08 15:27:02 -1000

    changes to basic, further editions of ch7..

--- basic.cpp
+++ basic.cpp
@@ -12,7 +12,10 @@ class Sales_Data
 
 public:
   //~~~~~~CONSTRUCTORS~~~~~~
- 
+  Sales_Data() = default;
+  Sales_Data(const std::string &s) : bookNo(s) { };
+  Sales_Data(const std::string &s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(n*p) { };
+   
   //~~MEMBER FUNCTIONS - functions defined here are implicitly inline..
   std::string isbn() const { return bookNo; };
   Sales_Data& combine(const Sales_Data&);
@@ -30,6 +33,7 @@ private:
   
 };
 
+
 double Sales_Data::avg_price() const {
   if (units_sold)
     return revenue / units_sold;
@@ -44,13 +48,19 @@ Sales_Data& Sales_Data::combine(const Sales_Data & rhs){
 }
 
 class Person {
+
 public:
+  Person() = default;
+  Person(const string &nm) : name(nm) { };
+  Person(const string &nm, const string &addr) : name(nm), address(addr) { };
+  Person(istream &is);
+
   //note that the below are const member functions as const follows the function declaration..
   //const member functions modify this identify that the parameters of the person class they are called on won't change..
   string getName() const { return this->name; }
   string getAddress() const { return this->address; }
-  void setName(string nme) {this->address = name;}
-  void setAddress(string addr) {this->address = address;}
+  void setName(string nme) {this->name = nme;}
+  void setAddress(string addr) {this->address = addr;}
 
 private:
   string name;
@@ -58,56 +68,75 @@ private:
 
 };
 
+  istream& read_person(istream &is, Person &p){
+    string name, addr;
+    printf("who are you?..\t");
+    is >> name;
+    p.setName(name);
+    printf("where do you live?.. ");
+    is >> addr;
+    p.setAddress(addr);
+   
+    return is;
+  }
+  
+  Person::Person(istream &is){
+    read_person(is, *this);
+  }
   // use this badboy to fill information about a person..
-  istream &read(istream &is, Person p)
-  {
-    string name;
-    string address;
-    is >> name >> address;
-    return is;      
+
+
+  ostream& print_person(ostream &os, Person &p){
+    os << "address is: " << p.getAddress() << endl;
+    os << "name is: " << p.getName() << endl;
+
+    return os;
   }
 
+  int main(int argc, char **argv)
+  {
+    //7.1 defining abstract data types
+    // some key words to know:
+    // interface        - things available to the user to interact with a class
+    // implementation   - data members / member functions of a class that include private and public memebers..
+    // data abstraction - refers to a programming technique which relies on interface and implementation..
+    // encapsulation    - enforces the separation of a class' interface and implementation
 
+    // cout << "hey there! " << endl;
+    // Sales_Data first;
+    // cout << "enters how many units you sold: ";
+    // cin >> first.units_sold;
+    // cout << "\nyou sold: " << first.units_sold << " number of books.. \n";
+    // for (int i = 0; i < first.units_sold; i++)
+    // {
+    //   cin >> first.revenue;
+    // }
+    // cout << "you have made: " << first.revenue << " monies!\n";
 
+    //7.1.2
+    //7.2 - include the above member function definitions..
+    //7.3 - revise previous program steps..
+    // Sales_Data second;
+    // printf("enter the following:\n isbn: ");
+    // cin >> second.bookNo;
+    // printf("\nunits sold: ");
+    // cin >> second.units_sold;
+    // printf("\nrevenue: ");
+    // cin >> second.revenue;
+    // printf("avg value is: ");
+    // cout << second.avg_price();
+    // second = second.combine(second);
+    // printf("\ndouble that revenue is:");
+    // cout << second.revenue;
 
-int main(int argc, char **argv)
-{
-  //7.1 defining abstract data types
-  // some key words to know:
-  // interface        - things available to the user to interact with a class
-  // implementation   - data members / member functions of a class that include private and public memebers..
-  // data abstraction - refers to a programming technique which relies on interface and implementation..
-  // encapsulation    - enforces the separation of a class' interface and implementation
+    //7.5 - create your own person class..
+    Person you("name");
+    read_person(cin, you);
+    print_person(cout, you);
 
-  // cout << "hey there! " << endl;
-  // Sales_Data first;
-  // cout << "enters how many units you sold: ";
-  // cin >> first.units_sold;
-  // cout << "\nyou sold: " << first.units_sold << " number of books.. \n";
-  // for (int i = 0; i < first.units_sold; i++)
-  // {
-  //   cin >> first.revenue;
-  // }
-  // cout << "you have made: " << first.revenue << " monies!\n";
+    //constructors
+    Person me(cin);
+    print_person(cout, me);
 
-  //7.1.2
-  //7.2 - include the above member function definitions..
-  //7.3 - revise previous program steps..
-  // Sales_Data second;
-  // printf("enter the following:\n isbn: ");
-  // cin >> second.bookNo;
-  // printf("\nunits sold: ");
-  // cin >> second.units_sold;
-  // printf("\nrevenue: ");
-  // cin >> second.revenue;
-  // printf("avg value is: ");
-  // cout << second.avg_price();
-  // second = second.combine(second);
-  // printf("\ndouble that revenue is:");
-  // cout << second.revenue; 
-
-  //7.5 - create your own person class..
-  
-  
-  return 0;
+    return 0;
 }
